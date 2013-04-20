96aa5a77c40fedd6f6371bfce4943842729ec008
Muhammed Nazim <mnm9994u@gmail.com>
2013-04-20 17:05:52 +0200

    UNOFFICIAL Builds are for Developers and Testers!
    This commits enables the foll. by default
    - Developer Options
    - Android Debugging
    - Advanced Reboot [Hide On Lockscreen]
    
    Change-Id: Ibef84a86143545e7cd7cbc591bbf94e9bc5e001e
    
    Conflicts:
    	src/com/android/settings/DevelopmentSettings.java

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -166,6 +166,7 @@ public class DevelopmentSettings extends PreferenceFragment
     private boolean mLastEnabledState;
     private boolean mHaveDebugSettings;
     private boolean mDontPokeProperties;
+    private boolean mUnofficialBuild;
 
     private CheckBoxPreference mEnableAdb;
     private CheckBoxPreference mAdbNotify;
@@ -235,6 +236,8 @@ public class DevelopmentSettings extends PreferenceFragment
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+
+        mUnofficialBuild = android.os.Build.VERSION.CODENAME.equals("UNOFFICIAL");
 
         mWindowManager = IWindowManager.Stub.asInterface(ServiceManager.getService("window"));
         mBackupManager = IBackupManager.Stub.asInterface(
@@ -500,7 +503,7 @@ public class DevelopmentSettings extends PreferenceFragment
         final ContentResolver cr = context.getContentResolver();
         mHaveDebugSettings = false;
         updateCheckBox(mEnableAdb, Settings.Global.getInt(cr,
-                Settings.Global.ADB_ENABLED, 0) != 0);
+                Settings.Global.ADB_ENABLED, mUnofficialBuild ? 1 : 0) != 0);
         mAdbNotify.setChecked(Settings.Secure.getInt(cr,
                 Settings.Secure.ADB_NOTIFY, 1) != 0);
         updateCheckBox(mBugreportInPower, Settings.Secure.getInt(cr,
@@ -747,7 +750,8 @@ public class DevelopmentSettings extends PreferenceFragment
 
     private boolean enableVerifierSetting() {
         final ContentResolver cr = getActivity().getContentResolver();
-        if (Settings.Global.getInt(cr, Settings.Global.ADB_ENABLED, 0) == 0) {
+        if (Settings.Global.getInt(cr, Settings.Global.ADB_ENABLED,
+                    mUnofficialBuild ? 1 : 0) == 0) {
             return false;
         }
         if (Settings.Global.getInt(cr, Settings.Global.PACKAGE_VERIFIER_ENABLE, 1) == 0) {
@@ -774,7 +778,7 @@ public class DevelopmentSettings extends PreferenceFragment
         if ("user".equals(Build.TYPE)) {
             final ContentResolver resolver = getActivity().getContentResolver();
             final boolean adbEnabled = Settings.Global.getInt(
-                    resolver, Settings.Global.ADB_ENABLED, 0) != 0;
+                    resolver, Settings.Global.ADB_ENABLED, mUnofficialBuild ? 1 : 0) != 0;
             if (adbEnabled) {
                 mBugreport.setEnabled(true);
                 mBugreportInPower.setEnabled(true);
