e5f1ab3debeb3f142c777c3c7ce6e5f648e81dc6
cphelps76 <cphelps76@gmail.com>
2013-07-30 09:19:29 -0500

    Revert "this is Droid Concepts not Sliroms, move the java to the right place"
    This reverts commit 96454e1a526fb4d1f78410c404193b3f37f99e00.

--- src/com/android/settings/slim/StatusBarClockStyle.java
+++ src/com/android/settings/slim/StatusBarClockStyle.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Droid Concepts Project
+ * Copyright (C) 2012 Slimroms Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.cyanogenmod;
+package com.android.settings.slim;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
