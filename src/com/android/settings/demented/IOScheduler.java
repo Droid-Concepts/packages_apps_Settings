LineageOS/android_packages_apps_Settings

Settings: Forward port Performance settings
Change-Id: Ib510b35fc2763c6942737afa33873aa5d349a173

src/com/android/settings/cyanogenmod/IOScheduler.java
@@ -1,0 +1,124 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+//
+// I/O Scheduler Related Settings
+//
+public class IOScheduler extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    public static final String IOSCHED_PREF = "pref_io_sched";
+    public static final String IOSCHED_LIST_FILE = "/sys/block/mmcblk0/queue/scheduler";
+
+    public static final String SOB_PREF = "pref_io_sched_set_on_boot";
+
+    private static final String TAG = "IOScheduler";
+
+    private String mIOSchedulerFormat;
+
+    private ListPreference mIOSchedulerPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mIOSchedulerFormat = getString(R.string.io_sched_summary);
+
+        String[] availableIOSchedulers = new String[0];
+        String availableIOSchedulersLine;
+        int bropen, brclose;
+        String currentIOScheduler = null;
+
+        addPreferencesFromResource(R.xml.ioscheduler_settings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mIOSchedulerPref = (ListPreference) prefScreen.findPreference(IOSCHED_PREF);
+
+        /* I/O scheduler
+        Some systems might not use I/O schedulers */
+        if (!Utils.fileExists(IOSCHED_LIST_FILE) ||
+            (availableIOSchedulersLine = Utils.fileReadOneLine(IOSCHED_LIST_FILE)) == null) {
+            prefScreen.removePreference(mIOSchedulerPref);
+
+        } else {
+            availableIOSchedulers = availableIOSchedulersLine.replace("[", "").replace("]", "").split(" ");
+            bropen = availableIOSchedulersLine.indexOf("[");
+            brclose = availableIOSchedulersLine.lastIndexOf("]");
+            if (bropen >= 0 && brclose >= 0)
+                currentIOScheduler = availableIOSchedulersLine.substring(bropen + 1, brclose);
+
+            mIOSchedulerPref.setEntryValues(availableIOSchedulers);
+            mIOSchedulerPref.setEntries(availableIOSchedulers);
+            if (currentIOScheduler != null)
+                mIOSchedulerPref.setValue(currentIOScheduler);
+            mIOSchedulerPref.setSummary(String.format(mIOSchedulerFormat, currentIOScheduler));
+            mIOSchedulerPref.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        String availableIOSchedulersLine;
+        int bropen, brclose;
+        String currentIOScheduler;
+
+        super.onResume();
+
+        if (Utils.fileExists(IOSCHED_LIST_FILE) &&
+            (availableIOSchedulersLine = Utils.fileReadOneLine(IOSCHED_LIST_FILE)) != null) {
+            bropen = availableIOSchedulersLine.indexOf("[");
+            brclose = availableIOSchedulersLine.lastIndexOf("]");
+            if (bropen >= 0 && brclose >= 0) {
+                currentIOScheduler = availableIOSchedulersLine.substring(bropen + 1, brclose);
+                mIOSchedulerPref.setSummary(String.format(mIOSchedulerFormat, currentIOScheduler));
+            }
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String fname = "";
+
+        if (newValue != null) {
+            if (preference == mIOSchedulerPref) {
+                fname = IOSCHED_LIST_FILE;
+            }
+
+            if (Utils.fileWriteOneLine(fname, (String) newValue)) {
+                if (preference == mIOSchedulerPref) {
+                    mIOSchedulerPref.setSummary(String.format(mIOSchedulerFormat, (String) newValue));
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+}

src/com/android/settings/cyanogenmod/MemoryManagement.java
@@ -1,0 +1,137 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import java.io.File;
+
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class MemoryManagement extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    public static final String KSM_RUN_FILE = "/sys/kernel/mm/ksm/run";
+
+    public static final String KSM_PREF = "pref_ksm";
+
+    public static final String KSM_PREF_DISABLED = "0";
+
+    public static final String KSM_PREF_ENABLED = "1";
+
+    private static final String ZRAM_PREF = "pref_zram_size";
+
+    private static final String ZRAM_PERSIST_PROP = "persist.service.zram"; // was compcache
+
+    private static final String ZRAM_DEFAULT = SystemProperties.get("ro.zram.default"); // was compcache
+
+    private static final String PURGEABLE_ASSETS_PREF = "pref_purgeable_assets";
+
+    private static final String PURGEABLE_ASSETS_PERSIST_PROP = "persist.sys.purgeable_assets";
+
+    private static final String PURGEABLE_ASSETS_DEFAULT = "0";
+
+    private ListPreference mzRAM;
+
+    private CheckBoxPreference mPurgeableAssetsPref;
+
+    private CheckBoxPreference mKSMPref;
+
+    private int swapAvailable = -1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getPreferenceManager() != null) {
+
+            addPreferencesFromResource(R.xml.memory_management);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+            mzRAM = (ListPreference) prefSet.findPreference(ZRAM_PREF);
+            mPurgeableAssetsPref = (CheckBoxPreference) prefSet.findPreference(PURGEABLE_ASSETS_PREF);
+            mKSMPref = (CheckBoxPreference) prefSet.findPreference(KSM_PREF);
+
+            if (isSwapAvailable()) {
+                if (SystemProperties.get(ZRAM_PERSIST_PROP) == "1")
+                    SystemProperties.set(ZRAM_PERSIST_PROP, ZRAM_DEFAULT);
+                mzRAM.setValue(SystemProperties.get(ZRAM_PERSIST_PROP, ZRAM_DEFAULT));
+                mzRAM.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mzRAM);
+            }
+
+            if (Utils.fileExists(KSM_RUN_FILE)) {
+                mKSMPref.setChecked(KSM_PREF_ENABLED.equals(Utils.fileReadOneLine(KSM_RUN_FILE)));
+            } else {
+                prefSet.removePreference(mKSMPref);
+            }
+
+            String purgeableAssets = SystemProperties.get(PURGEABLE_ASSETS_PERSIST_PROP,
+                    PURGEABLE_ASSETS_DEFAULT);
+            mPurgeableAssetsPref.setChecked("1".equals(purgeableAssets));
+
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        if (preference == mPurgeableAssetsPref) {
+            SystemProperties.set(PURGEABLE_ASSETS_PERSIST_PROP,
+                    mPurgeableAssetsPref.isChecked() ? "1" : "0");
+            return true;
+        }
+
+        if (preference == mKSMPref) {
+            Utils.fileWriteOneLine(KSM_RUN_FILE, mKSMPref.isChecked() ? "1" : "0");
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mzRAM) {
+            if (newValue != null) {
+                SystemProperties.set(ZRAM_PERSIST_PROP, (String) newValue);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if swap support is available on the system
+     */
+    private boolean isSwapAvailable() {
+        if (swapAvailable < 0) {
+            swapAvailable = new File("/proc/swaps").exists() ? 1 : 0;
+        }
+        return swapAvailable > 0;
+    }
+}

src/com/android/settings/cyanogenmod/PerformanceSettings.java
@@ -1,0 +1,113 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.ListPreference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+/**
+ * Performance Settings
+ */
+public class PerformanceSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+    private static final String TAG = "PerformanceSettings";
+
+    private static final String USE_DITHERING_PREF = "pref_use_dithering";
+
+    private static final String USE_DITHERING_PERSIST_PROP = "persist.sys.use_dithering";
+
+    private static final String USE_DITHERING_DEFAULT = "1";
+
+    private static final String USE_16BPP_ALPHA_PREF = "pref_use_16bpp_alpha";
+
+    private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
+
+    private ListPreference mUseDitheringPref;
+
+    private CheckBoxPreference mUse16bppAlphaPref;
+
+    private AlertDialog alertDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getPreferenceManager() != null) {
+
+            addPreferencesFromResource(R.xml.performance_settings);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+            String useDithering = SystemProperties.get(USE_DITHERING_PERSIST_PROP, USE_DITHERING_DEFAULT);
+            mUseDitheringPref = (ListPreference) prefSet.findPreference(USE_DITHERING_PREF);
+            mUseDitheringPref.setOnPreferenceChangeListener(this);
+            mUseDitheringPref.setValue(useDithering);
+            mUseDitheringPref.setSummary(mUseDitheringPref.getEntry());
+
+            mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
+            String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
+            mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
+
+            /* Display the warning dialog */
+            alertDialog = new AlertDialog.Builder(getActivity()).create();
+            alertDialog.setTitle(R.string.performance_settings_warning_title);
+            alertDialog.setMessage(getResources().getString(R.string.performance_settings_warning));
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,
+                    getResources().getString(com.android.internal.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            return;
+                        }
+                    });
+
+            alertDialog.show();
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mUse16bppAlphaPref) {
+            SystemProperties.set(USE_16BPP_ALPHA_PROP,
+                    mUse16bppAlphaPref.isChecked() ? "1" : "0");
+        } else {
+            // If we didn't handle it, let preferences handle it.
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mUseDitheringPref) {
+            String newVal = (String) newValue;
+            int index = mUseDitheringPref.findIndexOfValue(newVal);
+            SystemProperties.set(USE_DITHERING_PERSIST_PROP, newVal);
+            mUseDitheringPref.setSummary(mUseDitheringPref.getEntries()[index]);
+        }
+        return true;
+    }
+
+}

Droid-Concepts/packages_apps_Settings

Conflicts:
	AndroidManifest.xml
	res/values/arrays.xml
	res/values/strings.xml

src/com/android/settings/demented/IOScheduler.java
@@ -1,0 +1,124 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.demented;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+//
+// I/O Scheduler Related Settings
+//
+public class IOScheduler extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    public static final String IOSCHED_PREF = "pref_io_sched";
+    public static final String IOSCHED_LIST_FILE = "/sys/block/mmcblk0/queue/scheduler";
+
+    public static final String SOB_PREF = "pref_io_sched_set_on_boot";
+
+    private static final String TAG = "IOScheduler";
+
+    private String mIOSchedulerFormat;
+
+    private ListPreference mIOSchedulerPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mIOSchedulerFormat = getString(R.string.io_sched_summary);
+
+        String[] availableIOSchedulers = new String[0];
+        String availableIOSchedulersLine;
+        int bropen, brclose;
+        String currentIOScheduler = null;
+
+        addPreferencesFromResource(R.xml.ioscheduler_settings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mIOSchedulerPref = (ListPreference) prefScreen.findPreference(IOSCHED_PREF);
+
+        /* I/O scheduler
+        Some systems might not use I/O schedulers */
+        if (!Utils.fileExists(IOSCHED_LIST_FILE) ||
+            (availableIOSchedulersLine = Utils.fileReadOneLine(IOSCHED_LIST_FILE)) == null) {
+            prefScreen.removePreference(mIOSchedulerPref);
+
+        } else {
+            availableIOSchedulers = availableIOSchedulersLine.replace("[", "").replace("]", "").split(" ");
+            bropen = availableIOSchedulersLine.indexOf("[");
+            brclose = availableIOSchedulersLine.lastIndexOf("]");
+            if (bropen >= 0 && brclose >= 0)
+                currentIOScheduler = availableIOSchedulersLine.substring(bropen + 1, brclose);
+
+            mIOSchedulerPref.setEntryValues(availableIOSchedulers);
+            mIOSchedulerPref.setEntries(availableIOSchedulers);
+            if (currentIOScheduler != null)
+                mIOSchedulerPref.setValue(currentIOScheduler);
+            mIOSchedulerPref.setSummary(String.format(mIOSchedulerFormat, currentIOScheduler));
+            mIOSchedulerPref.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        String availableIOSchedulersLine;
+        int bropen, brclose;
+        String currentIOScheduler;
+
+        super.onResume();
+
+        if (Utils.fileExists(IOSCHED_LIST_FILE) &&
+            (availableIOSchedulersLine = Utils.fileReadOneLine(IOSCHED_LIST_FILE)) != null) {
+            bropen = availableIOSchedulersLine.indexOf("[");
+            brclose = availableIOSchedulersLine.lastIndexOf("]");
+            if (bropen >= 0 && brclose >= 0) {
+                currentIOScheduler = availableIOSchedulersLine.substring(bropen + 1, brclose);
+                mIOSchedulerPref.setSummary(String.format(mIOSchedulerFormat, currentIOScheduler));
+            }
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String fname = "";
+
+        if (newValue != null) {
+            if (preference == mIOSchedulerPref) {
+                fname = IOSCHED_LIST_FILE;
+            }
+
+            if (Utils.fileWriteOneLine(fname, (String) newValue)) {
+                if (preference == mIOSchedulerPref) {
+                    mIOSchedulerPref.setSummary(String.format(mIOSchedulerFormat, (String) newValue));
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+}

src/com/android/settings/demented/MemoryManagement.java
@@ -1,0 +1,137 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.demented;
+
+import java.io.File;
+
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class MemoryManagement extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    public static final String KSM_RUN_FILE = "/sys/kernel/mm/ksm/run";
+
+    public static final String KSM_PREF = "pref_ksm";
+
+    public static final String KSM_PREF_DISABLED = "0";
+
+    public static final String KSM_PREF_ENABLED = "1";
+
+    private static final String ZRAM_PREF = "pref_zram_size";
+
+    private static final String ZRAM_PERSIST_PROP = "persist.service.zram"; // was compcache
+
+    private static final String ZRAM_DEFAULT = SystemProperties.get("ro.zram.default"); // was compcache
+
+    private static final String PURGEABLE_ASSETS_PREF = "pref_purgeable_assets";
+
+    private static final String PURGEABLE_ASSETS_PERSIST_PROP = "persist.sys.purgeable_assets";
+
+    private static final String PURGEABLE_ASSETS_DEFAULT = "0";
+
+    private ListPreference mzRAM;
+
+    private CheckBoxPreference mPurgeableAssetsPref;
+
+    private CheckBoxPreference mKSMPref;
+
+    private int swapAvailable = -1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getPreferenceManager() != null) {
+
+            addPreferencesFromResource(R.xml.memory_management);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+            mzRAM = (ListPreference) prefSet.findPreference(ZRAM_PREF);
+            mPurgeableAssetsPref = (CheckBoxPreference) prefSet.findPreference(PURGEABLE_ASSETS_PREF);
+            mKSMPref = (CheckBoxPreference) prefSet.findPreference(KSM_PREF);
+
+            if (isSwapAvailable()) {
+                if (SystemProperties.get(ZRAM_PERSIST_PROP) == "1")
+                    SystemProperties.set(ZRAM_PERSIST_PROP, ZRAM_DEFAULT);
+                mzRAM.setValue(SystemProperties.get(ZRAM_PERSIST_PROP, ZRAM_DEFAULT));
+                mzRAM.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mzRAM);
+            }
+
+            if (Utils.fileExists(KSM_RUN_FILE)) {
+                mKSMPref.setChecked(KSM_PREF_ENABLED.equals(Utils.fileReadOneLine(KSM_RUN_FILE)));
+            } else {
+                prefSet.removePreference(mKSMPref);
+            }
+
+            String purgeableAssets = SystemProperties.get(PURGEABLE_ASSETS_PERSIST_PROP,
+                    PURGEABLE_ASSETS_DEFAULT);
+            mPurgeableAssetsPref.setChecked("1".equals(purgeableAssets));
+
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        if (preference == mPurgeableAssetsPref) {
+            SystemProperties.set(PURGEABLE_ASSETS_PERSIST_PROP,
+                    mPurgeableAssetsPref.isChecked() ? "1" : "0");
+            return true;
+        }
+
+        if (preference == mKSMPref) {
+            Utils.fileWriteOneLine(KSM_RUN_FILE, mKSMPref.isChecked() ? "1" : "0");
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mzRAM) {
+            if (newValue != null) {
+                SystemProperties.set(ZRAM_PERSIST_PROP, (String) newValue);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if swap support is available on the system
+     */
+    private boolean isSwapAvailable() {
+        if (swapAvailable < 0) {
+            swapAvailable = new File("/proc/swaps").exists() ? 1 : 0;
+        }
+        return swapAvailable > 0;
+    }
+}

src/com/android/settings/demented/PerformanceSettings.java
@@ -1,0 +1,113 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.demented;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.ListPreference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+/**
+ * Performance Settings
+ */
+public class PerformanceSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+    private static final String TAG = "PerformanceSettings";
+
+    private static final String USE_DITHERING_PREF = "pref_use_dithering";
+
+    private static final String USE_DITHERING_PERSIST_PROP = "persist.sys.use_dithering";
+
+    private static final String USE_DITHERING_DEFAULT = "1";
+
+    private static final String USE_16BPP_ALPHA_PREF = "pref_use_16bpp_alpha";
+
+    private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
+
+    private ListPreference mUseDitheringPref;
+
+    private CheckBoxPreference mUse16bppAlphaPref;
+
+    private AlertDialog alertDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getPreferenceManager() != null) {
+
+            addPreferencesFromResource(R.xml.performance_settings);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+            String useDithering = SystemProperties.get(USE_DITHERING_PERSIST_PROP, USE_DITHERING_DEFAULT);
+            mUseDitheringPref = (ListPreference) prefSet.findPreference(USE_DITHERING_PREF);
+            mUseDitheringPref.setOnPreferenceChangeListener(this);
+            mUseDitheringPref.setValue(useDithering);
+            mUseDitheringPref.setSummary(mUseDitheringPref.getEntry());
+
+            mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
+            String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
+            mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
+
+            /* Display the warning dialog */
+            alertDialog = new AlertDialog.Builder(getActivity()).create();
+            alertDialog.setTitle(R.string.performance_settings_warning_title);
+            alertDialog.setMessage(getResources().getString(R.string.performance_settings_warning));
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,
+                    getResources().getString(com.android.internal.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            return;
+                        }
+                    });
+
+            alertDialog.show();
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mUse16bppAlphaPref) {
+            SystemProperties.set(USE_16BPP_ALPHA_PROP,
+                    mUse16bppAlphaPref.isChecked() ? "1" : "0");
+        } else {
+            // If we didn't handle it, let preferences handle it.
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mUseDitheringPref) {
+            String newVal = (String) newValue;
+            int index = mUseDitheringPref.findIndexOfValue(newVal);
+            SystemProperties.set(USE_DITHERING_PERSIST_PROP, newVal);
+            mUseDitheringPref.setSummary(mUseDitheringPref.getEntries()[index]);
+        }
+        return true;
+    }
+
+}