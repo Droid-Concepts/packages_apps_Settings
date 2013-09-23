willl0344/android_packages_apps_Settings

First pass at generic hardware tunables support
With support for vibrator intensity and kcal

Change-Id: Ice3444b31a9ab0a25cf25bebb18351e2c08a70c3

src/com/android/settings/SoundSettings.java
@@ -54,6 +54,8 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.VolumePanel;
 
+import com.android.settings.cyanogenmod.VibratorIntensity;
+
 import java.util.Date;
 import java.util.Calendar;
 import java.util.List;
@@ -96,6 +98,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String KEY_POWER_NOTIFICATIONS = "power_notifications";
     private static final String KEY_POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
     private static final String KEY_POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
+    private static final String KEY_VIBRATOR_INTENSITY = "vibrator_intensity";
 
     private static final String RING_MODE_NORMAL = "normal";
     private static final String RING_MODE_VIBRATE = "vibrate";
@@ -349,6 +352,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
                 Settings.Global.POWER_NOTIFICATIONS_VIBRATE, 0) != 0);
         if (vibrator == null || !vibrator.hasVibrator()) {
             removePreference(KEY_POWER_NOTIFICATIONS_VIBRATE);
+            removePreference(KEY_VIBRATOR_INTENSITY);
+        }
+
+        if (!VibratorIntensity.isSupported()) {
+            removePreference(KEY_VIBRATOR_INTENSITY);
         }
 
         mPowerSoundsRingtone = findPreference(KEY_POWER_NOTIFICATIONS_RINGTONE);

Droid-Concepts/packages_apps_Settings

Conflicts:
	AndroidManifest.xml
	res/values/cm_strings.xml
	res/xml/display_settings.xml
	res/xml/sound_settings.xml
	src/com/android/settings/DisplaySettings.java
	src/com/android/settings/SoundSettings.java

src/com/android/settings/SoundSettings.java
@@ -50,6 +50,8 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.VolumePanel;
 
+import com.android.settings.demented.VibratorIntensity;
+
 import java.util.Date;
 import java.util.Calendar;
 import java.util.List;
@@ -84,11 +86,10 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String KEY_DOCK_AUDIO_MEDIA_ENABLED = "dock_audio_media_enabled";
     private static final String KEY_HEADSET_CONNECT_PLAYER = "headset_connect_player";
     private static final String KEY_QUIET_HOURS = "quiet_hours";
-
-
     private static final String SILENT_MODE_OFF = "off";
     private static final String SILENT_MODE_VIBRATE = "vibrate";
     private static final String SILENT_MODE_MUTE = "mute";
+    private static final String KEY_VIBRATOR_INTENSITY = "vibrator_intensity";
 
     private static final String[] NEED_VOICE_CAPABILITY = {
             KEY_RINGTONE, KEY_DTMF_TONE, KEY_CATEGORY_CALLS,
@@ -209,6 +210,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         if (vibrator == null || !vibrator.hasVibrator()) {
             removePreference(KEY_VIBRATE);
             removePreference(KEY_HAPTIC_FEEDBACK);
+            removePreference(KEY_VIBRATOR_INTENSITY);
+        }
+
+        if (!VibratorIntensity.isSupported()) {
+            removePreference(KEY_VIBRATOR_INTENSITY);
         }
 
         if (TelephonyManager.PHONE_TYPE_CDMA == activePhoneType) {
@@ -538,4 +544,3 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         return ab.create();
     }
 }
-